8e9df1cc09742e359cb836b28d89cc2402f5f311
Walera Kubasov <walerakubasov1985@yandex.ru>
2023-01-26 14:06:41 +0300

    ПР-4 MESTO(2)

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+let popup = document.querySelector('.popup');
+let buttenOpen = document.querySelector('.profile__author-btn');
+let buttenClose = document.querySelector('.popup__button-close');
+
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('.form__input_name');
+let jobInput = document.querySelector('.form__input_job');
+
+let profileAuthor = document.querySelector('.profile__author');
+let profileAuthorSubtitle = document.querySelector('.profile__author-subtitle');
+
+//подключение класса с dyspley:block//
+function popupOpened() {
+  popup.classList.add('popup_opened');
+};
+
+//отключение класса с dyspley:block//
+function popupClosed() {
+  popup.classList.remove('popup_opened');
+};
+
+//заполняем форму содержиым профиля//
+function textInput() {
+  nameInput.value = profileAuthor.textContent;
+  jobInput.value = profileAuthorSubtitle.textContent;
+};
+
+//Меняем содержиым профиля//
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileAuthor.textContent = nameInput.value;
+  profileAuthorSubtitle.textContent = jobInput.value
+  popupClosed()
+}
+
+textInput();
+buttenOpen.addEventListener('click', popupOpened);
+buttenClose.addEventListener('click', popupClosed);
+formElement.addEventListener('submit', handleFormSubmit);
